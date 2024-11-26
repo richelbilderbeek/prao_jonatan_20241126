7346195c995512c1cba2aab1f2fba38109e6c7a0
Jonatan Kaj <jonkaj20@gmail.com>
2024-11-26 13:37:52 +0100

    Write values to csv file and then read it

--- code.py
+++ code.py
@@ -1,22 +1,36 @@
-from timeit import default_timer as timer
-from datetime import timedelta
-import timeit
 import time
+import csv
 
 def is_prime(num):
-    if num> 1:
-        for n in range(2,num):
+    if num > 1:
+        for n in range(2, num):
             if (num % n) == 0:
                 return False
         return True
     else:
         return False
-    
 
-for i in range(0, 100000):
+fields = ["Values"]
+rows = []
+
+for i in range(0, 100):
+    # Measure time
     start = time.perf_counter()
     is_prime(i)
     end = time.perf_counter()
-    print(end - start)
+    elapsed = round((end - start) * 1000, 2)
 
-# type: ignore
+    rows.append([str(elapsed)])
+
+# Open file for writing
+with open("is_prime_1.csv", "w", newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(fields)
+    csv_writer.writerows(rows)
+
+# Open file for reading and print its contents
+with open("is_prime_1.csv", "r") as file:
+    print(file.read())
+
+print("Done!")
+#type: ignore
