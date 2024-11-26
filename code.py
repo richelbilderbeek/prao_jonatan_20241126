b6ab57ffdcb4105c724fa541189a592db2b9b3c7
Jonatan Kaj <jonkaj20@gmail.com>
2024-11-26 13:01:41 +0100

    Works

--- code.py
+++ code.py
@@ -1,34 +1,22 @@
+from timeit import default_timer as timer
+from datetime import timedelta
+import timeit
 import time
-#test
 
-def isprime(num):
-  for n in range(
-    2, int(num**0.5)+1
-  ):
-    if num%n==0:
-      return False
-  return True
+def is_prime(num):
+    if num> 1:
+        for n in range(2,num):
+            if (num % n) == 0:
+                return False
+        return True
+    else:
+        return False
+    
 
-print("starting the calculation...")
-print(" ")
-z = 1
-for x in range(0, 1000000):
-    st = time.time()
+for i in range(0, 100000):
+    start = time.perf_counter()
+    is_prime(i)
+    end = time.perf_counter()
+    print(end - start)
 
-    isprime(z)
-    z += 1
-    for n in range(
-       2, int(z**0.5)+1
-     ):
-        if z%n==0:
-          et = time.time()
-          elapsed_time = round(et - st)
-          print(z,   "time:", elapsed_time)
-      
-et = time.time()
-elapsed_time = round(et - st)
-print(z,   "time:", elapsed_time)
-
-
-print(" ")
-print("finished!")
+# type: ignore
